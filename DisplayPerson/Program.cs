93a48e9c39ab7c572d9c6b73b81ff4987073b8e0
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-02-22 23:35:25 +1100

    Add project files.

---
+++ DisplayPerson/Program.cs
@@ -0,0 +1,34 @@
+﻿using BasicOop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayPerson
+{
+    internal class Program
+    {
+        //https://www.exercisescsharp.com/oop/first-class-method-tostring
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Person myPerson = new Person();
+            var personName = myPerson.Name;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Person name: ");
+                personName = Console.ReadLine();
+                list.Add(personName);
+            }
+            Console.WriteLine("\n\n");
+            foreach (var item in list)
+            {
+                Console.WriteLine($"Hello! My name is {item}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
